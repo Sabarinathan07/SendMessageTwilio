ede8dd2531a587d0b3cbf8d263d90bb1d8a49eae
Sabari <r.sabarinathan22@gmail.com>
2022-01-10 20:33:14 +0530

    send message

---
+++ index.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+	.create({
+		body: 'Hello this is sabari from twilio😂',
+		from: '+18646714648',
+		to: '+919445330575',
+	})
+	.then((message) => console.log(message.sid));
